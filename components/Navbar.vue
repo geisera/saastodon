46d773aa0cd8ff4614e25d51aae6e97403933fdd
Tony <geisera@gmail.com>
2023-01-09 07:27:47 -0600

    sticky navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,41 +1,102 @@
 <template>
-    <Disclosure as="nav" class="bg-white shadow" v-slot="{ open }">
+    <Disclosure as="nav" class="site-nav bg-white shadow" v-slot="{ open }">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 justify-between">
           <div class="flex">
             <div class="-ml-2 mr-2 flex items-center md:hidden">
-              <!-- Mobile menu button -->
-              <DisclosureButton class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <!-- Mobile menu button | hidden on desktop -->
+              <DisclosureButton
+                class="
+                  inline-flex 
+                  items-center 
+                  justify-center 
+                  rounded-md p-2 
+                  text-gray-400 
+                  hover:bg-gray-100 
+                  hover:text-gray-500 
+                  focus:outline-none 
+                  focus:ring-2 
+                  focus:ring-inset 
+                  focus:ring-indigo-500
+                  ">
                 <span class="sr-only">Open main menu</span>
                 <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
                 <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
               </DisclosureButton>
             </div>
-            <div class="flex flex-shrink-0 items-center">
+            <div 
+              class="
+                flex 
+                flex-shrink-0 
+                items-center
+            ">
               <h1>SAASTODON</h1>
             </div>
             <div class="hidden md:ml-6 md:flex md:space-x-8">
               <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-              <a href="/blog" class="inline-flex items-center border-b-2 border-red-500 px-1 pt-1 text-sm font-medium text-gray-900">News</a>
-              <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Team</a>
+              <a href="/blog" 
+                class="
+                inline-flex 
+                items-center 
+                border-b-2 
+                border-red-500 
+                px-1 pt-1 
+                text-sm 
+                font-medium 
+                text-gray-900
+                ">News</a>
+              <a href="#" 
+                class="
+                  inline-flex 
+                  items-center 
+                  border-b-2 
+                  border-transparent 
+                  px-1 
+                  pt-1 
+                  text-sm 
+                  font-medium 
+                  text-gray-500 
+                  hover:border-gray-300 
+                  hover:text-gray-700
+                  ">Team</a>
             </div>
           </div>
           <div class="flex items-center">
             <div class="flex-shrink-0">
-              <button type="button" class="relative inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <PlusIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-                <span>Join FREE</span>
+              <button type="button" 
+                class="
+                relative 
+                inline-flex 
+                items-center 
+                rounded-md 
+                border 
+                border-transparent 
+                bg-red-600 
+                px-4 
+                py-2 
+                text-sm 
+                font-medium 
+                text-white 
+                shadow-sm 
+                hover:bg-red-700 
+                focus:outline-none 
+                focus:ring-2 
+                focus:ring-indigo-500 
+                focus:ring-offset-2
+                ">
+                  <PlusIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                  <span>Join FREE</span>
               </button>
             </div>
           </div>
         </div>
       </div>
-  
+      <!-- hidden mobile navigation -->
       <DisclosurePanel class="md:hidden">
         <div class="space-y-1 pt-2 pb-3">
           <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
-          <DisclosureButton as="a" href="/blog" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6">News</DisclosureButton>
-          <DisclosureButton as="a" href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6">Team</DisclosureButton>
+          <DisclosureButton as="a" href="/blog" class="block border-l-4 border-red-500 bg-red-50 py-2 pl-3 pr-4 text-base font-medium text-red-700 sm:pl-5 sm:pr-6">News</DisclosureButton>
+          <DisclosureButton as="a" href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6">?</DisclosureButton>
         </div>
       </DisclosurePanel>
     </Disclosure>
@@ -43,7 +104,7 @@
 
 <script setup>
     import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-    import { MenuIcon, BellIcon, XIcon } from '@heroicons/vue/outline'
+    import { MenuIcon, XIcon } from '@heroicons/vue/outline'
     import { PlusIcon } from '@heroicons/vue/solid'
 </script>
 
